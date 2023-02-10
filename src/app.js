8a910aaf86792a6d99c759b17a0bedaa651c2531
equis23 <lammdevv@gmail.com>
2023-02-10 18:00:23 -0500

    able to sent messages and notify when a user enter or left the chat

--- src/app.js
+++ src/app.js
@@ -1,10 +1,29 @@
+const path = require('path');
 const express = require('express');
 const http = require('http');
+const { Server } = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
+const io = new Server(server);
 
-app.use(express.static('/public'));
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+io.on('connection', (socket) => {
+  io.emit('server:user-connected', socket.id);
+
+  socket.on('disconnect', () => {
+    io.emit('server:user-disconnected', socket.id);
+  });
+
+  socket.on('client:chat-message', (message) => {
+    io.emit('server:chat-message', message);
+  });
+});
 
 server.listen(8080, (err) => {
   if (err) return console.log(err);
